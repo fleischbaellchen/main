2db13d1e89e336982c4764d27f02860ff2d211e3
Zeno <zeno.koller@gmail.com>
2014-10-11 04:20:38 +0200

    Added Navigation Controller

--- testprojects/ProductTable/ProductTable/ViewController.swift
+++ testprojects/ProductTable/ProductTable/ViewController.swift
@@ -14,38 +14,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var products: [Product]! = []
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        /**************************/
-        // FOR TESTING
-        /**************************/
+    @IBAction func getProduct() {
         let EAN = "7617027097710"
         let url = NSURL(string: "http://api.autoidlabs.ch//products/\(EAN)")
         let request = NSURLRequest(URL: url)
-        
+    
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response, data, error) in
-            
-            //println(NSString(data: data, encoding: NSUTF8StringEncoding))
-            
-            // Handle no network error
-            if data == nil {
-                var alertController = UIAlertController(title: "Oops!", message: "Please check your network connection.", preferredStyle: .Alert)
-                var okAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
-                alertController.addAction(okAction)
-                self.presentViewController(alertController, animated: true, completion: nil)
-                return
-            }
-            
-            // Extract JSON and create Product
-            let json = JSON(data: data)
-            self.products.append(Product(data: json, ean: EAN))
-            
-            // Update table view
-            // self.tableView.reloadData()
+    
+                //println(NSString(data: data, encoding: NSUTF8StringEncoding))
+    
+                // Handle no network error
+                if data == nil {
+                    var alertController = UIAlertController(title: "Oops!", message: "Please check your network connection.", preferredStyle: .Alert)
+                    var okAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+                    alertController.addAction(okAction)
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                    return
+                }
+    
+                // Extract JSON and create Product
+                let json = JSON(data: data)
+                self.products.append(Product(data: json, ean: EAN))
+    
+                // Update table view
+                if let tableView = self.tableView {
+                    tableView.reloadData()
+                } else {
+                    println("No table view")
+                }
         })
+    }
 
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
     }
 
@@ -59,15 +63,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         if let array = products {
             return array.count
         }
-        return 0
+        return 10
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("productCell", forIndexPath: indexPath) as UITableViewCell
         
         let product: Product = self.products[indexPath.row]
-        let name = cell.viewWithTag(0) as UILabel
-        name.text = product.name
+        let nameLabel = cell.viewWithTag(1) as UILabel
+        nameLabel.text = product.name
 
         return cell
     }
