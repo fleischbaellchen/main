41e476221d417501b480f83ffba3ee44a57483aa
David Keller <dev@davidkeller.me>
2014-10-11 22:08:52 +0200

    Adds db folder with setup sql-script & er diagram with realtions

---
+++ db/setup_empty_db.sql
@@ -0,0 +1,27 @@
+-- Sets up a new database with the schema drawn in `ER_and_Schema.jpg`
+-- 
+-- David Keller, 11.10.2014
+--
+
+PRAGMA foreign_keys=ON; -- enforce foreign keys
+
+create table Category (
+    categoryName TEXT PRIMARY KEY
+);
+
+create table Product (
+    EAN             INTEGER PRIMARY KEY,
+    name            TEXT,
+    creationDate    INTEGER,
+    mainCategory    TEXT, 
+    FOREIGN KEY(mainCategory) REFERENCES Category(categoryName)
+);
+
+create table ShoppingList (
+    listId          INTEGER,
+    EAN             TEXT,
+    tickedOff       INTEGER,
+    addedDate       INTEGER,
+    PRIMARY KEY(listId, EAN),
+    FOREIGN KEY(EAN) REFERENCES Product(EAN)
+);
